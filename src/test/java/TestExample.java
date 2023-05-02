57e95d9db64ac36de3f0cc2375a78922b2c0da98
Lev Gorfel <lev.gorfel@basis.net>
2023-05-01 20:23:06 -0400

    Initial commit

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class TestExample {
+    static WebDriver webDriver;
+    static Logger logger = LogManager.getLogger(TestExample.class);
+    @BeforeAll
+    public static void setup() {
+        logger.info("BeforeAll");
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+    @AfterAll
+    public static void tearDown() {
+        logger.info("AfterAll");
+        webDriver.quit();
+    }
+
+
+    @Test
+    public void test() {
+        System.out.println("Test");
+        webDriver.get("https://www.google.com");
+        Assertions.assertEquals("Google", webDriver.getTitle());
+    }
+}
